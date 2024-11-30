e693ed7c32d2e5da9d54850ff5698803377e54c3
AC25-C <151057176+AC25-C@users.noreply.github.com>
2024-11-30 01:38:28 +0100

    <z

---
+++ src/main/java/com/example/alphasolution/AlphaSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphasolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaSolutionApplication.class, args);
+    }
+
+}
