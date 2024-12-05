eaaf58ea3d8784855c468e4dca470c838dd810c7
Arem Canbaz <151057176+AremCanbaz@users.noreply.github.com>
2024-12-05 05:04:39 +0100

    Update TaskRepository.java

--- src/main/java/com/example/alphasolution/Repository/TaskRepository.java
+++ src/main/java/com/example/alphasolution/Repository/TaskRepository.java
@@ -10,8 +10,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import static com.example.alphasolution.Repository.DbManager.connection;
-
 @Repository
 public class TaskRepository {
     Connection con = DbManager.getConnection();
@@ -41,33 +39,6 @@ public class TaskRepository {
         }
         return tasks;
     }
-
-    public void deleteTask(int taskId) {
-        String deleteQuery = "DELETE FROM tasks WHERE taskid = ?";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery);
-            preparedStatement.setInt(1, taskId);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-      
-    public String getTaskNameById(int taskId) {
-        String query = "SELECT taskname FROM tasks WHERE taskid = ?";
-        try {
-            PreparedStatement preparedStatement = con.prepareStatement(query);
-            preparedStatement.setInt(1, taskId);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    return resultSet.getString("taskname");
-                }
-
-            }
-        }
-        catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        }
-        return null;
     public void createTask(int subtaskid, String taskdescription, String taskname, int time) {
         String query1 = "INSERT INTO tasks (subtaskid, taskname, description, hoursspent) VALUES (?,?,?,?)";
         try {
@@ -79,6 +50,5 @@ public class TaskRepository {
             preparedStatement.executeUpdate();
         }  catch (SQLException sqlException) {
             sqlException.printStackTrace();}
-
     }
 }
